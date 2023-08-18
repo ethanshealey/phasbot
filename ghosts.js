2714e767afb9221283e82ff57d98c3e09e73dcca
ethanshealey <ethan.shealey@gmail.com>
2023-08-17 20:07:45 -0400

    cleaned up

--- ghosts.js
+++ ghosts.js
@@ -7,13 +7,16 @@ export default (evidence, notEvidence) => {
     if(!evidence?.length && !notEvidence?.length) return ghosts.all
 
     const possible = ghosts.all.filter((g) => {
+        // Remove ghosts that DONT have evidence
         for(let i = 0; i < evidence.length; i++) 
             if(!g.evidence.has(evidence[i]))
                 return false
+        // Remove ghosts that DO have rejected evidence
         for(let i = 0; i < notEvidence.length; i++) {
             if(g.evidence.has(notEvidence[i]))
                 return false
         }
+        // Valid Ghost
         return true
     })
 
